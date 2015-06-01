0a895b270397089566bfd3a110f1c47491f538de
Ruben Estrada <restrada82000@gmail.com>
2015-06-01 22:11:08 +0200

    apartado b

--- Prueba.java
+++ Prueba.java
@@ -6,13 +6,14 @@ import org.junit.Test;
 
 public class Prueba {
 	private static final int N = 5;
-	private BinaryString prueba1, prueba2;
-	private String pruebas2 = "10110";
+	private BinaryString prueba1, prueba2, prueba3;
+	private String pruebas2 = "10110", pruebas3 = "100101";
 	
 	@Before
 	public void iniciar() {
 		prueba1 = new BinaryString(N);
 		prueba2 = new BinaryString(pruebas2);
+		prueba3 = new BinaryString(pruebas3);
 	}
 	
 	@Test
@@ -20,4 +21,11 @@ public class Prueba {
 		assertEquals(N, prueba1.length());
 		assertEquals(N, prueba2.length());
 	}
+	
+	@Test
+	public void testSetBit() {
+		prueba3.set(1, '1');
+		assertEquals('1', prueba3.get(1));
+	}
+
 }
